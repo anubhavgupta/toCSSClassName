baa2e775cb226249ca2bd3b739d3ec4cdd8944b6
Anubhav Gupta <anubhav200@gmail.com>
2017-03-29 14:25:55 +0530

    performance improvements

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-// `c1, c2:${x}, c3, $:${y}, c5, c6`
-export default function $$C(...args) {
-  const [strArray, ...vars] = args;
-  let dataPointer = -1;
-  const allClasses = [];
-  strArray
-    .forEach((classString)=>{
-      let className = [];
-      classString = classString + ',';
-      // parser
-      classString
-      .split('')
-      .forEach(char => {
-        switch (char) {
-        case ',':
-          className.length ? allClasses.push(className.join('')): null;
-          className = [];
-          break;
-        case ':':
-          dataPointer++;
-          const condition = vars[dataPointer];
-          // check for '$' sign
-          let classNameCurrentPointer = className.length;
-          let classNameCurrentPointedChar = className[classNameCurrentPointer];
-          if(condition){
-            // we should apply this class.
-            allClasses.push(className.join(''));
-          }
-          className = []; // clear className
-          break;
-      case '$':
-            // replace the $ with className which itself is the variable value.
-            dataPointer++;
-            const classNameIsVar = (vars[dataPointer] && vars[dataPointer].toString) ? vars[dataPointer].toString().split('') : '$';
-            className = className.concat(classNameIsVar);
-            dataPointer--;
-            break;
-        default:
-          className.push(char);
-          break;
+// example: $$C`c1, c2:${i}, c3, c4-$:${i+1}, c5`;
+export default function $$C() {
+    var strArray = arguments[0];
+    var dataPointer = 0;
+    var allClasses = [];
+    var strArrayLength = strArray.length, classStrLength;
+    for(var i = 0, classString = strArray[0]; i < strArrayLength; i++, classString = strArray[i]) {
+        classStrLength = classString.length;
+        var stringStack = [], startPtr = -1, endPtr = -1;
+        for (var j=0, char=classString[0]; j < classStrLength; j++, char=classString[j]) {
+            switch (char) {
+                case ',':
+                    if(endPtr > startPtr){
+                        allClasses.push(classString.substring(startPtr, endPtr));
+                    }
+                    startPtr = -1;
+                    endPtr = -1;
+                    break;
+                case ':':
+                    dataPointer++;
+                    var condition = arguments[dataPointer];
+                    if (condition) {
+                        if(stringStack.length) { // todo handle case when user types `c1$` only and `c$1`
+                            // we have $ in the text
+                            var classNameIsVar = (condition && condition.toString) ? condition.toString() : '$';
+                            stringStack.push(endPtr > startPtr ? classString.substring(startPtr, endPtr) : '');
+                            var finalClassName = stringStack.join(classNameIsVar);
+                            allClasses.push(finalClassName);
+                        } else {
+                            (endPtr > startPtr) ? allClasses.push(classString.substring(startPtr, endPtr)): null;
+                        }
+                    }
+                    startPtr = -1;
+                    endPtr = -1;
+                    stringStack = [];// clear stack
+                    break;
+                case '$':
+                    // replace the $ with className which itself is the variable value.
+                    // partial classname so push to stack.
+                    (endPtr > startPtr)? stringStack.push(classString.substring(startPtr, endPtr)): null;
+                    startPtr = -1;
+                    endPtr = -1;
+                    break;
+                default:
+                    if(!~startPtr){
+                        startPtr = j;
+                        endPtr = j;
+                    }
+                    endPtr ++;
+                    break;
+            }
         }
-      });
-    });
+        // check if we still have some parsed data available, it must be a class name not ending with ,
+        (endPtr > startPtr) ? allClasses.push(classString.substring(startPtr, endPtr)) : null
+    }
     return allClasses.join(' ');
 }
